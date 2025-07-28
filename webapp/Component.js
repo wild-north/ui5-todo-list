91fdcf099d265448a308ffd17aec4a574de95242
Serhii Strizhak <serhii_strizhak@epam.com>
2025-07-28 14:57:39 +0300

    fix: Apply prettier formatting to generated JS files

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,46 +1,45 @@
-"use strict";
-sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/Device"
-], (UIComponent, JSONModel, Device) => {
-    "use strict";
-    return UIComponent.extend("ui5.todo.Component", {
-        metadata: {
-            interfaces: ["sap.ui.core.IAsyncContentCreation"],
-            manifest: "json"
-        },
-        init() {
-            UIComponent.prototype.init.apply(this, arguments);
-            const todoData = {
-                groups: [
-                    {
-                        id: "default",
-                        title: "My Tasks",
-                        color: "blue"
-                    }
-                ],
-                todos: [
-                    {
-                        id: "1",
-                        title: "Welcome to Todo List",
-                        description: "This is your first todo item. Click to edit or create new ones!",
-                        status: "todo",
-                        groupId: "default",
-                        createdAt: new Date().toISOString(),
-                        dueDate: null
-                    }
-                ]
-            };
-            const todoModel = new JSONModel(todoData);
-            this.setModel(todoModel, "todos");
-            const deviceModel = new JSONModel(Device);
-            deviceModel.setDefaultBindingMode("OneWay");
-            this.setModel(deviceModel, "device");
-            this.getRouter().initialize();
-        },
-        getContentDensityClass() {
-            return Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact";
-        }
+'use strict';
+sap.ui.define(
+  ['sap/ui/core/UIComponent', 'sap/ui/model/json/JSONModel', 'sap/ui/Device'],
+  (UIComponent, JSONModel, Device) => {
+    'use strict';
+    return UIComponent.extend('ui5.todo.Component', {
+      metadata: {
+        interfaces: ['sap.ui.core.IAsyncContentCreation'],
+        manifest: 'json',
+      },
+      init() {
+        UIComponent.prototype.init.apply(this, arguments);
+        const todoData = {
+          groups: [
+            {
+              id: 'default',
+              title: 'My Tasks',
+              color: 'blue',
+            },
+          ],
+          todos: [
+            {
+              id: '1',
+              title: 'Welcome to Todo List',
+              description: 'This is your first todo item. Click to edit or create new ones!',
+              status: 'todo',
+              groupId: 'default',
+              createdAt: new Date().toISOString(),
+              dueDate: null,
+            },
+          ],
+        };
+        const todoModel = new JSONModel(todoData);
+        this.setModel(todoModel, 'todos');
+        const deviceModel = new JSONModel(Device);
+        deviceModel.setDefaultBindingMode('OneWay');
+        this.setModel(deviceModel, 'device');
+        this.getRouter().initialize();
+      },
+      getContentDensityClass() {
+        return Device.support.touch ? 'sapUiSizeCozy' : 'sapUiSizeCompact';
+      },
     });
-});
+  }
+);
